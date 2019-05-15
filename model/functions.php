e9ff1654bc3c8028c6eec442a19175097e34dd43
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-05-14 21:44:21 -0400

    Removed unused features

--- model/functions.php
+++ model/functions.php
@@ -19,17 +19,11 @@ function validEmail($email){
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
-function validIndoor($indoor, $options){
-    foreach ($indoor as $key => $value){
-        if(!array_key_exists($value, $options)){
-            return false;
-        }
+function validArray($array, $options){
+    if(empty($indoor)){
+        return true;
     }
-    return true;
-}
-
-function validOutdoor($outdoor, $options){
-    foreach ($outdoor as $key => $value){
+    foreach ($indoor as $key => $value){
         if(!array_key_exists($value, $options)){
             return false;
         }
@@ -40,43 +34,24 @@ function validOutdoor($outdoor, $options){
 function validatePersonalInfo($f3){
     $errors = array();
 
-    //prem
-    $_SESSION['prem'] = isset($_POST['prem']);
-
     //fname
     if(!validName($_POST['fname'])){
         $errors['fnameERR'] = 'The first name is invalid';
-    }
-    else{
-        $_SESSION['fname'] = $_POST['fname'];
     }
 
     //lname
     if(!validName($_POST['lname'])){
         $errors['lnameERR'] = 'The last name is invalid';
     }
-    else{
-        $_SESSION['lname'] = $_POST['lname'];
-    }
 
     //age
     if(!validAge($_POST['age'])){
         $errors['ageERR'] = 'The age is invalid';
     }
-    else{
-        $_SESSION['age'] = $_POST['age'];
-    }
-
-    //gender
-    //no validation needed
-    $_SESSION['gender'] = $_POST['gender'];
 
     //phone
     if(!validPhone($_POST['phone'])){
         $errors['phoneERR'] = 'The phone number is invalid';
-    }
-    else{
-        $_SESSION['phone'] = $_POST['phone'];
     }
 
     $f3->set('personalInfoERRS', $errors);
@@ -90,16 +65,6 @@ function validateProfileEntry($f3){
     if(!validEmail($_POST['email'])){
         $errors['emailERR'] = 'You need to enter an email';
     }
-    else{
-        $_SESSION['email'] = $_POST['email'];
-    }
-
-    $_SESSION['state'] = $_POST['state'];
-
-    $seeking = $_POST['seeking'];
-    $_SESSION['seeking'] = $seeking[0];
-
-    $_SESSION['bio'] = $_POST['bio'];
 
     $f3->set('profileEntryERRS', $errors);
     return $errors;
@@ -108,40 +73,26 @@ function validateProfileEntry($f3){
 function validateInterests($f3){
     $indoorVals = $f3->get('indoor');
     $outdoorVals = $f3->get('outdoor');
-    return ((validIndoor($_POST['indoor'], $indoorVals))&&(validOutdoor($_POST['outdoor'], $outdoorVals)));
-}
-
-function storeInterests($f3){
-
-    $indoor = $f3->get('indoor');
-    $outdoor = $f3->get('outdoor');
-
-    $newInterests = "";
-    foreach ($_POST['indoor'] as $key => $value){
-        $newInterests .=  $indoor[$value]. ", ";
-    }
-    foreach ($_POST['outdoor'] as $key => $value){
-        $newInterests .= $outdoor[$value] . ", ";
-    }
-
-    $_SESSION['interests'] = substr_replace($newInterests, "", -2);
+    return ((validArray($_POST['indoor'], $indoorVals))&&(validArray($_POST['outdoor'], $outdoorVals)));
 }
 
 function stringifyOneArray(array $array, array $starter){
     $string = '';
     //loop over the array and grab the necessary conversion in the original array
     foreach ($array as $key => $value){
+        //make sure the key is in there
         if(array_key_exists($value, $starter)){
+            //add the conversion to the final string
             $string .= $starter["$value"] . ', ';
         }
     }
 
+    //fix the fencepost issue
     return substr_replace($string, "", -2);
 }
 
 function stringifyTwoArrays(array $array1, array $array2, array $starter1, array $starter2){
     $string = '';
-    //loop over each array and append the conversions to the final string
     $string .= stringifyOneArray($array1, $starter1);
     $string .= ', ';
     $string .= stringifyOneArray($array2, $starter2);
